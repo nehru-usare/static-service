a2fbb4066879e5569a7c784577c2121170a82a88
Nehru Usare <113748523+nehru-usare@users.noreply.github.com>
2025-03-15 18:44:33 +0530

    Add files via upload

---
+++ src/main/java/com/smartjava/siteservice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.smartjava.siteservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SiteServiceApplication.class);
+	}
+
+}
---
+++ src/test/java/com/smartjava/staticweb/StaticWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartjava.staticweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
